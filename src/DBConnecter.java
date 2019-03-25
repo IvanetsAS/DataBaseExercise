0ae945145fdca781aaa6f5a40f4fef96f7220508
ivanetsas <ivanetsas@yandex.ru>
2019-03-25 12:29:54 +0300

    Adding menu and adding content to db methods

--- src/DBConnecter.java
+++ src/DBConnecter.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.*;
 
 /**
  * Simple Java program to connect to MySQL database running on localhost and
@@ -12,48 +13,113 @@ public class DBConnecter {
     private static final String user = "u0518408_studydb";
     private static final String password = "u0518408_studydb";
 
+    private static final String createMessagesTable = "CREATE TABLE IF NOT EXISTS messages (\n" +
+            "    id INT primary key AUTO_INCREMENT,\n" +
+            "    text TEXT NOT NULL,\n" +
+            "    idSend INT,\n" +
+            "    idGet INT,\n" +
+            "    dateSend DATE" +
+            ")";
+
+    private static final String createUsersTable = "CREATE TABLE IF NOT EXISTS users (\n" +
+            "    id INT AUTO_INCREMENT,\n" +
+            "    name TEXT NOT NULL,\n" +
+            "    PRIMARY KEY (id)" +
+            ")";
+
+    private static final String insertNewUser = "INSERT INTO users VALUES(?, ?);";
+
+
+
     // JDBC variables for opening and managing connection
     private static Connection connection;
     private static Statement stmt;
     private static ResultSet rs;
 
+    private static Scanner in;
+
     public static void main(String args[]) {
-        String query = "select count(*) from books";
+        createDataBaseTables();
 
-        String createMessagesTableQuery = "CREATE TABLE IF NOT EXISTS messages (\n" +
-                "    id INT primary key AUTO_INCREMENT,\n" +
-                "    text TEXT NOT NULL,\n" +
-                "    idSend INT,\n" +
-                "    idGet INT,\n" +
-                "    dateSend DATE" +
-                ")";
+        in = new Scanner(System.in);
 
-        String createUsersTableQuery = "CREATE TABLE IF NOT EXISTS users (\n" +
-                "    id INT AUTO_INCREMENT,\n" +
-                "    name TEXT NOT NULL,\n" +
-                "    PRIMARY KEY (id)" +
-                ")";
+        String menuString = "My console application main menu. To do activity choose and write its number:\n" +
+                "1 - Add new user" + "\n" +
+                "2 - quite";
+        System.out.println(menuString);
 
-        String insertNewUserQuery = "INSERT INTO users VALUES(?);";
+        loop: while (in.hasNext()){
+            int command = in.nextInt();
+            switch (command){
+                case 1:
+                    setNewUser();
+                    break loop;
+                case 2:
+                    System.out.println("Bye!");
+                    break loop;
+            }
+            System.out.println(menuString);
 
+        }
+    }
+
+    private static void setNewUser(){
+        Map<Integer, String> userData = new HashMap<>();
+
+        System.out.println("Write User id");
+        int userId = in.nextInt();
+
+        System.out.println("Write User Name");
+        String userName = in.nextLine();
+
+
+        userData.put(userId, userName);
+//
+//        insertIntoTable(insertNewUser, new HashMap<Integer, String>(){{
+//            put(1, userName);
+//            put(2, userName);
+//        }});
+
+        System.out.println(userData);
+    }
+
+    private static void insertIntoTable(String queryStatement, Map<Integer, String> dataList){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             connection = DriverManager.getConnection(url, user, password);
             stmt = connection.createStatement();
 
-            PreparedStatement preparedCreateMessagesTableStatement = connection.prepareStatement(createMessagesTableQuery);
+            PreparedStatement preparedStatement = connection.prepareStatement(queryStatement);
+
+            for(Map.Entry<Integer, String> e : dataList.entrySet())
+                preparedStatement.setString(e.getKey(), e.getValue());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            try { connection.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+    }
+
+    private static void createDataBaseTables() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(url, user, password);
+            stmt = connection.createStatement();
+
+            PreparedStatement preparedCreateMessagesTableStatement = connection.prepareStatement(createMessagesTable);
             preparedCreateMessagesTableStatement.executeUpdate();
 
-            PreparedStatement preparedCreateUsersTablesStatement = connection.prepareStatement(createUsersTableQuery);
+            PreparedStatement preparedCreateUsersTablesStatement = connection.prepareStatement(createUsersTable);
             preparedCreateUsersTablesStatement.executeUpdate();
 
-            PreparedStatement preparedInsertNewMessageStatement = connection.prepareStatement(insertNewUserQuery);
-            preparedInsertNewMessageStatement.setString(1, "usr1");
-            preparedInsertNewMessageStatement.executeUpdate();
-
             //int res = stmt.executeUpdate(createMessagesTableQuery);
-            System.out.println("Connected successful");
+            System.out.println("Successfully connected to database");
 
             // "INSERT message (title1 title2 title3) VALUES (? ? ?)"
             // SELECT id, name FROM users WHERE id=?
@@ -73,7 +139,7 @@ public class DBConnecter {
             //close connection ,stmt and resultset here
             try { connection.close(); } catch(SQLException se) { /*can't do anything */ }
             try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+            //try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
         }
     }
 
